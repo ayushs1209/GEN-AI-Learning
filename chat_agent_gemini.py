632edb02b4feda5355f495416841bb6ac42cc629
ayushs1209 <ayushsus1209@gmail.com>
2025-04-29 16:07:56 +0530

    update

--- chat_agent_gemini.py
+++ chat_agent_gemini.py
@@ -18,7 +18,7 @@ you can make a hybrid system and use the multiple tools given to you and on your
 **important instruction for weather tool** : if the user gives a generic location of something like a whole state or something you must calculate the weather of the nearest point from the given location and if the user asks you to tell the weather about the whole country then you should just calculate the weather for the either popular places in the country or the capital of the country after asking the user for their approval
 
 **important instruction for Run command tool**
-You can use this tool to execute commands and make changes to the files on the system using the run_a_command function which executes the command using the os.system(command),so you can use it to write a complete script that interfaces with web search engines or external libraries for detailed results and make complete directories and files based on the user input aided by your knowledge. 
+You can use this tool to execute commands and make changes to the files on the system using the run_a_command function which executes the command using the os.system(command),so you can use it to write a complete Python script that interfaces with web search engines or external libraries for detailed results and make complete directories and files based on the user input aided by your knowledge. 
 
 Example 1 :
 user: what is the weather in Jammu
@@ -26,16 +26,12 @@ model: the user wants to ask for the weather in jammu
 model: I have a tool that lets me calculate the weather in a location
 model: i should pass the location to the function and get the result as the weather statitics of the place
 model: generate the output
-
-
 Example 2 :
 user: what is the average of the weather of jammu mumbai and patiala
 model: user wants me to calculate the average of the weathers of jammu mumbai and patiala
 model: i should call the tool to fetch the weather and keep the results
 model: now I can average the numerical values of the weather outputs of the different locations
 model: now I should return the average weather of the three states and generate the output as the user asked me to
-
-
 Example 3 :
 user: can you make a python file which adds two numbers
 model: user wants me to creat a file so i should use the run command tool to make the file sum.py to add the code in the file
@@ -43,14 +39,12 @@ model: now that I have made the file I should add the code to the file and also 
 model: a.b = input("enter numbers you want to add") return a+b is the code you added to the file using the run_command_tool
 model: now you should check if the code you added is correct or not
 model: after checking and making the validation prompt the user that the file is made
-
-
 Example 4 :
 user:  I want you to make a folder in which you should make some files on all the file handling operations in python  in their seperate file each and also I want you to generate working code of each file with enough comments to explain the worlflow of the code
 model: the user wants me to make a folder in which the user want to have all the file operation in python in their each seperate files
 model: now firstly i'll use mkdir to make a new folder named file_operations and in this folder Ill add all the different file named after each file operations available in python such as read, write, append, etc. 
 model: now after creating each file I will have to add the corresponding code in each the folder to its function 
-model: after making each file with working well generated code now I must check the code in each the file if it is working correctly or not. if not I must make the cahnges to make it working completely fine according to the user prompts
+model: after making each file with working well generated code now I must check the code in each the file if it is workign correctly or not. if not I must make the cahnges to make it working completely fine according to the user prompts
 model: now that all's done. I should prompt the user tha all the operations requested have been done succesfully
 
 
